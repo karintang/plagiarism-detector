e0bb137d4492aba1cd23757bf6a53fb4bd49d88e
karintang <77315323+karintang@users.noreply.github.com>
2023-03-31 13:30:29 +0800

    catch error

--- app/plagiarism_detector.py
+++ app/plagiarism_detector.py
@@ -11,15 +11,33 @@ final_model_name = 'final_model.tar.gz'
 ngrams_lst = [1,4,5]
 
 def lambda_handler(event, context):
-    if
-    input_doc = event['input_doc']
-    input_doc_name = event['input_doc_name']
-    source_docs = event['source_docs']
-    
-    response = get_matching_output(directory, s3_bucket, s3_folderpath, sentbert_model_name, final_model_name, ngrams_lst, source_docs, input_doc, input_doc_name)
-
-    response_object = {}
-    response_object['statusCode'] = 200
-    response_object['body'] = json.dumps(response)
-    
-    return response_object
+   try:
+       input_doc = event['input_doc']
+       input_doc_name = event['input_doc_name']
+       source_docs = event['source_docs']
+       response = get_matching_output(directory, s3_bucket, s3_folderpath, sentbert_model_name, final_model_name, ngrams_lst, source_docs, input_doc, input_doc_name)
+       response_object = {}
+       response_object['statusCode'] = 200
+       response_object['body'] = json.dumps(response)
+       return { 
+               response_object
+               }
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BadRequestException':
+            # Handle the 400 Bad Request error here
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Bad Request: ' + str(e))
+            }
+        else:
+            # Handle other ClientErrors
+            return {
+                'statusCode': 500,
+                'body': json.dumps('Internal Server Error: ' + str(e))
+            }
+    except Exception as e:
+        # Handle other Exceptions
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Internal Server Error: ' + str(e))
+        }
